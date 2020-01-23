68d30f57e49f2f0f0e1fa62aaffe55f5b7d45dbf
Alex Zheng <20850333+prototype-A@users.noreply.github.com>
2020-01-22 19:55:44 -0500

    First commit: Created project, imported tilemap and character spirtes, created tile platform for testing and implemented basic player controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+    public float movementSpeed = 5.0f;
+    public float jumpForce = 300.0f;
+
+    private Rigidbody2D rigidBody;
+    private bool isGrounded = true;
+
+    private void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        float movementX = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+
+        // Player Jump
+        if (Input.GetButtonDown("Jump")) {
+            if (isGrounded) {
+                isGrounded = false;
+                rigidBody.AddForce(transform.up * jumpForce);
+            }
+        }
+
+        // Player hits ground
+        if (rigidBody.velocity[1] == 0) {
+            isGrounded = true;
+        } else {
+            isGrounded = false;
+        }
+
+        transform.Translate(new Vector3(movementX, 0.0f, 0.0f));
+    }
+}
